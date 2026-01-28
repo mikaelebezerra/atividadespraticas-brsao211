625f1bf57c49ed1ac9f63e008a4a51da42f845e8
mikaelebezerra <mikaeleb30@gmail.com>
2026-01-28 18:58:30 -0300

    atividade 4 - questao 3

---
+++ questao03.py
@@ -0,0 +1,31 @@
+while True:
+    try:
+        senha = input("Digite a senha: ")
+
+        if senha.lower() == 'sair':
+         break
+      
+        if len(senha) < 8:
+         raise Exception("A senha deve ter pelo menos 8 caracteres")
+        
+        tem_numero = False
+        for caractere in senha:
+           
+            if caractere in '0123456789':
+                tem_numero = True
+                break
+
+        if tem_numero == False:
+           raise Exception("A senha deve conter pelo menos um número")
+        
+        print("Senha válida")
+        break
+
+
+
+    except Exception as e:
+       print(f"Erro: {e} Tente novamente! ")
+       
+
+
+   
