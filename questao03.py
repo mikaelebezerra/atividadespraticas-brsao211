fe21810597e3dc31b233ec9ed908ca990b0a4fdb
mikaelebezerra <mikaeleb30@gmail.com>
2026-01-30 16:13:21 -0300

    atividade 6 - questao 3

---
+++ questao03.py
@@ -0,0 +1,25 @@
+import requests
+
+def consultar_cep(cep):
+    url = f"https://viacep.com.br/ws{cep}/json/"
+    try:
+        resposta = requests.get(url)
+        dados = resposta.json()
+
+        if 'erro' in dados:
+            return "CEP inválido"
+
+        logradouro = dados['logradouro']
+        bairro = dados['bairro']
+        localidade = dados['localidade']
+        uf = dados['uf']
+
+        return f"Logradouro: {logradouro}\nBairro: {bairro}\nLocalidade: {localidade}\nUF: {uf}"
+    
+    except requests.RequestException as e:
+        return f"Erro ao consultar CEP: {e}"
+    
+
+cep = input("Digite o CEP: ")
+resultado = consultar_cep(cep)
+print(resultado)
